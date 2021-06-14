339c7463bd77a3dc4c3d0b82c9ba22177f2037dc
cscreativestar <me@creative-star.co.uk>
2021-06-14 13:31:29 +0100

    added API call for daily forecast

--- src/index.js
+++ src/index.js
@@ -26,9 +26,9 @@ function showCity(event) {
   searchCity(enterCityInput.value);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
-
   let forecastHTML = `<div class="row">`;
   let days = [
     "Tuesday",
@@ -58,7 +58,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast();
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "f7da78bd04741d407fc9d96cf87b54b8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={part}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function searchCity(city) {
   let apiKey = "f7da78bd04741d407fc9d96cf87b54b8";
@@ -69,7 +75,6 @@ function searchCity(city) {
 let enterCityForm = document.querySelector("#city-form");
 enterCityForm.addEventListener("submit", showCity);
 function showTemperature(response) {
-  console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = `${temperature}˚C`;
@@ -87,6 +92,7 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   celsiusTemperature = response.data.main.temp;
+  getForecast(response.data.coord);
 }
 
 function revealPosition(position) {
